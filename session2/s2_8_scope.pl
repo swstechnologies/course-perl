ba8a891c625f452a51bc799d975f3a0f025f6283
Khelender Sasan <khelender.sasan@gmail.com>
2025-04-07 23:45:43 +0530

    - Naming convention updated to avoid confusion and better understanding  - Numbering done for comparison purpose
     On branch main Changes to be committed:
    	modified:   session2/s2_8_scope.pl

--- session2/s2_8_scope.pl
+++ session2/s2_8_scope.pl
@@ -3,27 +3,27 @@
 # use warnings;
 
 # Global variable
-our $global_var = "I am a global variable";
+our $global_var = "I am a global variable(#1) declared at TOP";
 
 sub demo_local_scope() {
 
-    print "\tInside second level nested subroutine:\n";
-    print "\tlexical_var = $lexical_var\n";
+    print "\tInside second level nested subroutine demo_local_scope():\n";
+    print "\tblock_scope_var = $block_scope_var\n";
     print "\tdynamic_var => $dynamic_var\n";
     print "\tglobal_var => $global_var\n";
  }
 
 sub demonstrate_scope {
 
-    print "Inside subroutine:\n";
+    print "Inside demonstrate_scope() subroutine:\n";
     print "global_var => $global_var\n";
 
-    # Lexical variable with 'my'
-    my $lexical_var = "I am a lexical variable";
-    our $dynamic_var = "I am a dynamic variable";
-    local $global_var = "I am a locally scoped global variable";
+    # Block Scope variable with 'my'
+    my $block_scope_var = "I am a BLOCK scope variable(#2)";
+    our $dynamic_var = "I am a dynamic variable(#3)";
+    local $global_var = "I am a locally scoped variable(#4) that over-rides GLOBAL global_var ";
 
-    print "lexical_var => $lexical_var\n";
+    print "block_scope_var => $block_scope_var\n";
     print "global_var => $global_var\n";
     print "dynamic_var => $dynamic_var\n";
 
@@ -32,15 +32,15 @@ sub demonstrate_scope {
     print "==========\n";
 }
 
-print "Before / Outside subroutine:\n";
+print "Before / Outside demonstrate_scope() subroutine:\n";
 print "global_var => $global_var\n";
 print "==========\n";
 
 demonstrate_scope();
 
-print "After subroutine call:\n";
+print "After subroutine demonstrate_scope() call:\n";
 print "global_var => $global_var\n";
 
-# Attempt to print lexical variable outside its scope (will cause an error)
-# print "$lexical_var\n"; # Uncommenting this line will cause an error
+# Attempt to print BLOCK scope variable outside its scope (will cause an error)
+# print "$block_scope_var\n"; # Uncommenting this line will cause an error
 
